64673ca73f5c4395e52dd8de5e81917541ed3555
Joey Dreijer <jdreijer@specterops.io>
2026-05-24 00:47:54 +0200

    Add config, exec, pull and start

--- src/apple_compose/commands/__init__.py
+++ src/apple_compose/commands/__init__.py
@@ -4,9 +4,13 @@ import importlib
 def register_commands() -> None:
     for module_name in (
         "apple_compose.commands.build.main",
+        "apple_compose.commands.config.main",
         "apple_compose.commands.up.main",
         "apple_compose.commands.down.main",
         "apple_compose.commands.ps.main",
+        "apple_compose.commands.pull.main",
+        "apple_compose.commands.exec.main",
+        "apple_compose.commands.start.main",
         "apple_compose.commands.stop.main",
         "apple_compose.commands.restart.main",
         "apple_compose.commands.logs.main",
